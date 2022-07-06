7de46b9d46086c7d7957888246904854a6fe0bd7
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-07-06 08:45:18 -0400

    Renamed title (to test deployment setup)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1>React Weather App</h1>
       </header>
     </div>
   );
