f7e21db46f2655526410a8e2a52f27d4f987c3a3
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-07-13 07:36:38 -0400

    Removed unnecessary React files

--- src/Search.js
+++ src/Search.js
@@ -10,6 +10,7 @@ export default function Search() {
           type="search"
           placeholder="Enter a city"
           autoComplete="off"
+          autoFocus="on"
         />
         <input className="search-button borders" type="submit" value="Search" />
         <button className="current-button borders" type="button">
