ef179a2f921526f8b91a52b28ac412507c66a1ed
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-08-07 11:15:11 -0400

    Added return value to map function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -33,6 +33,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastHour data={dailyForcast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
